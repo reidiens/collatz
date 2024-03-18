e2d06146c6a419552915960a87e1018643c7c860
Ruben Toledo <tibamita@tutamail.com>
2024-03-18 00:14:11 -0400

    did some light work no reaction

--- collatz.c
+++ collatz.c
@@ -13,134 +13,113 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+//#include <errno.h>
 #include <time.h>
-#include <errno.h>
 
-unsigned char ulget(unsigned long *num);                    // get the user input (ulget = unsigned long get)
-void collatz(unsigned long num, FILE *stream);              // do the conjecture & print it
-unsigned char savequery();                                  // process whether the user wants to output to file
-unsigned char savedata(unsigned long num);                  // output to file
-unsigned char checkargs(char *argv[], unsigned long *num);  // check the command line arguments
+typedef struct Array {
+    int cap;
+    int size;
+    unsigned long *arr;
+    float elapsed_ms;
+} arr_t;
+
+unsigned long checkargs(char *argv[], unsigned long *num);
+unsigned char ulget(unsigned long *num);
+int sixtypercent(int num);
+arr_t collatz(unsigned long num);
 
 int main(int argc, char *argv[]) {
     unsigned long num;
-
     if (!checkargs(argv, &num)) {
-        printf("input a number: ");
+        puts("input the starting value:");
         while (!ulget(&num))
-            puts("try again:");
+            puts("try again. hint: it has to be a positive, non-zero integer");
     }
 
-    collatz(num, stdout);
+    arr_t a = collatz(num);
 
-    puts("\nsave to a file? (y/n)");
-
-    if (!savequery()) return 0;
-
-    if (!savedata(num)) return -1;
-        
-    return 0;
-}
-
-unsigned char ulget(unsigned long *num) {
-    char temp[64];      // 64 is arbitrary
-    long temp_num;
-    fgets(temp, sizeof(temp), stdin);
-
-    if (strlen(temp) > 1) {
-        if (sscanf(temp, "%lu", &temp_num)) {
-            if (temp_num <= 0) return 0;
-            *num = temp_num;
-            return 1;
-        }
-        return 0;
-    }
-    return 0;
-}
-
-void collatz(unsigned long num, FILE *stream) {
-    unsigned long temp = num;
-    int count = 0;
-
-    fprintf(stream, "%lu\n", temp);
-
-    clock_t time = clock();
-    float elapsed_time = 0;
-
-    while (temp > 1) {
-        if (temp % 2) {
-            temp = temp * 3 + 1;
-
-            time = clock() - time;
-            elapsed_time = elapsed_time + (((float)time) / CLOCKS_PER_SEC);
-
-            count++;
-        }
-        else {
-            temp /= 2;
-
-            time = clock() - time;
-            elapsed_time = elapsed_time + (((float)time) / CLOCKS_PER_SEC);
-
-            count++;
-        }
-        fprintf(stream, "%lu\n", temp);
-    }
-    float elapsed_ms = elapsed_time * 1000;
-
-    fprintf(stream, "\nthe number %lu takes %d iterations to reach 1.\n", num, count);
-
-    if (elapsed_time * 1000 >= 250)     // 250 is arbitrary XD
-        fprintf(stream, "it took %f milliseconds (%f seconds) to finish iterating.\n", elapsed_ms, elapsed_time);
-
-    else 
-        fprintf(stream, "it took %f milliseconds (%f microseconds) to finish iterating.\n", elapsed_ms, elapsed_ms * 1000);
-
-}
-
-unsigned char savequery() {
-    char ans = getchar();
+    puts(" ");
     
-    switch (ans) {
-        case '\n':
-        case 'Y':
-        case 'y': return 1;
-        default: return 0;
-    }
+    for (int i = 0; i < a.size; i++)
+        printf("%lu\n", a.arr[i]);
+
+    printf("\nthe number %lu takes %d iterations to reach 1.\n", a.arr[0], a.size - 1);
+    printf("it took a total of %f milliseconds to finish calulations\n", a.elapsed_ms);
+
+    return 0;
 }
 
-unsigned char savedata(unsigned long num) {
-    FILE *fp = fopen("collatz_data", "w");
-    if (fp == NULL) {
-        perror(strerror(errno));
-        return 0;
-    }
-
-    clock_t time = clock();
-    collatz(num, fp);
-    time = clock() - time;
-
-    float elapsed_ms = ( ((float)time) / CLOCKS_PER_SEC ) * 1000;
-    printf("\nfile saved successfully :)\n");
-    printf("it took %f milliseconds (%f microseconds) to write the output file.\n\n", elapsed_ms, elapsed_ms * 1000);
-
-    if (fclose(fp)) {
-        perror(strerror(errno));
-        return 0;
-    }
-
+unsigned long checkargs(char *argv[], unsigned long *num) {
+    if (argv[1] == NULL) return 0;
+    
+    if (!sscanf(argv[1], "%lu", num)) return 0;
     return 1;
 }
 
-unsigned char checkargs(char *argv[], unsigned long *num) {
-    if (argv[1] == NULL) return 0;
-    
-    unsigned long temp;
-    if (sscanf(argv[1], "%lu", &temp)) {
-        if (temp <= 0) return 0;
-        *num = temp;
-        return 1;
+
+unsigned char ulget(unsigned long *num) {
+    char temp[64];
+    fgets(temp, sizeof(temp), stdin);
+
+    if (strlen(temp) > 1) {
+        if (!sscanf(temp, "%lu", num)) return 0;
+        else return 1;
     }
-    return 0;
+    else return 0;
+}
+
+int sixtypercent(int num) {
+    float temp = (float)num;
+    return (int)(temp * 0.6);
+}
+
+arr_t collatz(unsigned long num) {
+    arr_t a;
+
+    a.cap = 5;
+    a.size = 0;
+    a.arr = malloc(a.cap * sizeof(unsigned long));
+    a.arr[0] = num;
+    a.size++;
+    a.elapsed_ms = 0;
+
+    clock_t time = clock();
+
+    while (num != 1) {
+        if (num % 2) {
+            num = num * 3 + 1;
+
+            time = clock() - time;
+            a.elapsed_ms = a.elapsed_ms + ( ((float)time) / CLOCKS_PER_SEC ) * 1000;
+
+            a.arr[a.size] = num;
+            a.size++;
+
+            if (a.size >= sixtypercent(a.cap)) {
+                a.cap *= 2;
+                a.arr = realloc(a.arr, a.cap * sizeof(unsigned long));
+            }
+        }
+        else {
+            num /= 2;
+
+            time = clock() - time;
+            a.elapsed_ms = a.elapsed_ms + ( ((float)time) / CLOCKS_PER_SEC) * 1000;
+
+            a.arr[a.size] = num;
+            a.size++;
+
+            if (a.size >= sixtypercent(a.cap)) {
+                a.cap *= 2;
+                a.arr = realloc(a.arr, a.cap * sizeof(unsigned long));
+            }
+        }
+    }
+
+    a.cap = a.size;
+    a.arr = realloc(a.arr, a.cap * sizeof(unsigned long));
+
+    return a;
 }
